29b2b2efabaa44163c34f3b56d548b225b8dbb2d
Alejandro <d0tplist@gmail.com>
2020-06-23 10:08:48 -0500

    updated package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftHouse",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftHouse",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "SwiftHouseTests",
             dependencies: ["SwiftHouse"]),
